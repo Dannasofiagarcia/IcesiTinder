f497f44c2b6613a37d32d9a996cb62e4fe3ac901
kamneklogs <kamneklogs@gmail.com>
2020-11-28 08:59:01 -0500

    fixed adjMatrix

--- src/data_structures/WeightedGraph/Graph.java
+++ src/data_structures/WeightedGraph/Graph.java
@@ -10,21 +10,18 @@ import java.util.Map;
 public class Graph<V> {
 
 	private Map<Integer, Node<V>> adjList;
-	private final int defaultNumNodes = 5;
-	private int[][] nodesAdjMatrix;
-	private int numVertices;
+	private int nV;
+
 	private int numEdges;
+	public int[][] adjMatrix;
 
 	public Graph() {
 
 		adjList = new HashMap<Integer, Node<V>>();
-		numVertices = 0;
 		numEdges = 0;
-		// nodesAdjMatrix = new int[defaultNumNodes][defaultNumNodes];
-		/*
-		 * for (int i = 0; i < nodesAdjMatrix.length; i++) { for (int j = 0; j <
-		 * nodesAdjMatrix[i].length; j++) { nodesAdjMatrix[i][j] = 0; } }
-		 */
+		nV = 0;
+
+		adjMatrix = new int[nV][nV];
 	}
 
 	public Map<Integer, Node<V>> getNodes() {
@@ -37,21 +34,22 @@ public class Graph<V> {
 
 	public void addNode(V value) {
 
-		if (nodesAdjMatrix == null)
-			nodesAdjMatrix = new int[numVertices][numVertices];
+		int[][] newMatrix = new int[adjMatrix.length + 1][adjMatrix.length + 1];
+
+		for (int i = 0; i < adjMatrix.length; i++) {
+			for (int j = 0; j < adjMatrix.length; j++) {
+				newMatrix[i][j] = adjMatrix[i][j];
+			}
+
+		}
+
+		adjMatrix = newMatrix;
 
 		adjList.put(value.hashCode(), new Node<V>(value));
 
-		addNodeAdjMatrix();
-		adjList.get(value.hashCode()).setIndexMatrix(numVertices - 1);
-	}
+		nV = nV + 1;
 
-	public int[][] getNodesAdjMatrix() {
-		return nodesAdjMatrix;
-	}
-
-	public void setNodesAdjMatrix(int[][] nodesAdjMatrix) {
-		this.nodesAdjMatrix = nodesAdjMatrix;
+		adjList.get(value.hashCode()).setIndexMatrix(nV - 1);
 	}
 
 	public void addConnection(Integer hashCodeOrigin, Integer hashCodeDestination, int weight) {
@@ -59,8 +57,8 @@ public class Graph<V> {
 		origin.addEdge(new Edge<V>(origin, destination, weight));
 		destination.addEdge(new Edge<V>(destination, origin, weight));
 
-		nodesAdjMatrix[origin.getIndexMatrix()][destination.getIndexMatrix()] = weight;
-		nodesAdjMatrix[destination.getIndexMatrix()][origin.getIndexMatrix()] = weight;
+		adjMatrix[origin.getIndexMatrix()][destination.getIndexMatrix()] = weight;
+		adjMatrix[destination.getIndexMatrix()][origin.getIndexMatrix()] = weight;
 
 	}
 
@@ -82,54 +80,8 @@ public class Graph<V> {
 	}
 
 	public List<Node<V>> dijkstraForAdjaMatrix(Integer hashCodeOrigin, Integer hashCodeDestination) {
-
-		int o = adjList.get(hashCodeOrigin).getIndexMatrix(), d = adjList.get(hashCodeOrigin).getIndexMatrix();
-
-		DijkstraAlgorithmForAdjacencyMatrix dijkstra = new DijkstraAlgorithmForAdjacencyMatrix();
-
-		int[][] newMatrix = new int[nodesAdjMatrix.length - 1][nodesAdjMatrix.length - 1];
-
-		for (int i = 0; i < newMatrix.length - 1; i++) {
-			for (int j = 0; j < newMatrix.length - 1; j++) {
-				newMatrix[i][j] = nodesAdjMatrix[i][j];
-			}
-		}
-		int[][] miGrafo = newMatrix;
-		miGrafo[2][4] = 3;
-		miGrafo[4][2] = 3;
-		for (int x = 0; x < miGrafo.length; x++) {
-			System.out.print("|");
-			for (int y = 0; y < miGrafo[x].length; y++) {
-				System.out.print(miGrafo[x][y]);
-				if (y != miGrafo[x].length - 1)
-					System.out.print("\t");
-			}
-			System.out.println("|");
-		}
-		dijkstra.dijkstra(newMatrix, o, d, numVertices);
-
-		List<Integer> pathWihtIndices = dijkstra.getShortesPath();
-		List<Node<V>> temporalNodes = new ArrayList<>();
-
-		temporalNodes.addAll(adjList.values());
-
-		List<Node<V>> shortestPath = new ArrayList<>();
-		// System.out.println("tamaño temporales" + temporalNodes.size() + " tamaño
-		// camino " + pathWihtIndices.size());
-
-		for (int i = 0; i < temporalNodes.size(); i++) {
-			if (i == temporalNodes.size() - 1)
-				shortestPath = this.dijkstraForAdjaList(hashCodeOrigin, hashCodeDestination);
-			for (int j = pathWihtIndices.size(); j < pathWihtIndices.size(); j++) {
-				System.out.println("entrooo");
-
-				if (temporalNodes.get(i).getIndexMatrix() == (int) pathWihtIndices.get(j)) {
-					shortestPath.add(temporalNodes.get(i));
-				}
-			}
-		}
-
-		return shortestPath;
+		// hajhsnkjsankjdsnkjsdnjkdss
+		return null;
 
 	}
 
@@ -158,14 +110,6 @@ public class Graph<V> {
 
 	}
 
-	public int getNumVertices() {
-		return numVertices;
-	}
-
-	public void setNumVertices(int numVertices) {
-		this.numVertices = numVertices;
-	}
-
 	public int getNumEdges() {
 		return numEdges;
 	}
@@ -174,61 +118,4 @@ public class Graph<V> {
 		this.numEdges = numEdges;
 	}
 
-	public void addNodeAdjMatrix() {
-
-		numVertices++;
-
-		int v = getNumVertices();
-		if (v >= nodesAdjMatrix.length) {
-			int[][] newNodesAdjMatrix = new int[v + 1][v + 1];
-			for (int i = 0; i < nodesAdjMatrix.length; i++) {
-				for (int j = 0; j < nodesAdjMatrix.length; j++) {
-					newNodesAdjMatrix[i][j] = nodesAdjMatrix[i][j];
-
-				}
-			}
-			nodesAdjMatrix = newNodesAdjMatrix;
-		}
-		for (int i = 0; i < nodesAdjMatrix[v].length; i++) {
-			nodesAdjMatrix[v][i] = 0;
-			nodesAdjMatrix[i][v] = 0;
-		}
-
-		for (int i = 0; i < nodesAdjMatrix.length; i++) {
-			for (int j = 0; j < nodesAdjMatrix[i].length; j++) {
-				if (nodesAdjMatrix[i][j] == 0) {
-					nodesAdjMatrix[i][j] = 0;
-				}
-			}
-		}
-	}
-
-	public int distance[] = new int[numVertices];
-	public int cost[][] = new int[10][10];
-
-	public void dijkstra2(int n, int s) {
-		int flag[] = new int[n + 1];
-		int i, minpos = 1, k, c, minimum;
-
-		for (i = 1; i <= n; i++) {
-			flag[i] = 0;
-			this.distance[i] = this.nodesAdjMatrix[s][i];
-		}
-		c = 2;
-		while (c <= n) {
-			minimum = 99;
-			for (k = 1; k <= n; k++) {
-				if (this.distance[k] < minimum && flag[k] != 1) {
-					minimum = this.distance[i];
-					minpos = k;
-				}
-			}
-			flag[minpos] = 1;
-			c++;
-			for (k = 1; k <= n; k++) {
-				if (this.distance[minpos] + this.nodesAdjMatrix[minpos][k] < this.distance[k] && flag[k] != 1)
-					this.distance[k] = this.distance[minpos] + this.nodesAdjMatrix[minpos][k];
-			}
-		}
-	}
 }
