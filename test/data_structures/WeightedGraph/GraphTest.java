d8747b4599cb20877803048e0c9f33be696d4394
Danna <Dannasofiagarciatrujillo@hotmail.com>
2020-12-03 10:44:06 -0500

    test fixed

--- test/data_structures/WeightedGraph/GraphTest.java
+++ test/data_structures/WeightedGraph/GraphTest.java
@@ -5,9 +5,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class GraphTest {
 
